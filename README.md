f2044f76ac4b91ce4f60742f47787fe9cfb20084
Ignacio T <nax_hh@hotmail.com>
2012-04-26 12:42:49 +0200

    neo4j + readme

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-playground
-==========
+# Playground
 
-A site to play with new features
+Playground is just my personal site to make proof of concept and test some new features that i'm considering add to some project or i just find them great.
+
+
+## Setup instructions
+
+I recommend don't use them :P
+Each folder is a test and i try to keep codes well commented.
+Never know when gonna be usefull for anyone.
+
+## Requirements
+	Requirements depends on each test.
+
+## Tests
+
+* neo4j
+Neo4J is a test for the neo4j api in nodejs.
+You need a `neo4j database` (i'm using 1.7 on windows) to use this test and node-neo4j
+	npm install neo4j
---
+++ neo4j/app.js
@@ -0,0 +1,50 @@
+/*
+ *	Testing the graph database Neo4J with nodeJS
+ *
+ *	
+ */
+
+var neo4js = require('neo4j');
+
+//create the graphdatabase
+var db = new neo4js.GraphDatabase( process.env.NEO4J_URL || 'http://localhost:7474');
+
+
+//create nodes
+var bcn = db.createNode({'stop' : 'Barcelona'});
+var reus = db.createNode({'stop' : 'Reus'});
+var tgn = db.createNode({'stop' : 'Tarragona'});
+
+//create relationships
+bcn.createRelationshipTo(tgn, 'goes');
+tgn.createRelationshipTo(reus, 'goes');
+
+//save all into the db
+/* ToDO: Find better way to resolve this */
+tgn.save(function(err) { 
+	reus.save(function() { 
+
+		bcn.save(function(err) {
+			console.log('nodes saved');
+
+			//get the node
+			/*db.getNodeById(1, function(err, res) {
+				console.log(res.data.stop);
+			});*/
+
+
+			//get the path fron reus to barcelona
+			neo4js.path(reus, bcn, function(err, res) {
+				console.log(res);
+			});
+
+
+			//delete all nodes
+			/* ToDO: find a better way*/
+			bcn.delete();
+			tgn.delete();
+			reus.delete();
+		});
+
+	});
+});
