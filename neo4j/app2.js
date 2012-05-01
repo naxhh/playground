8f43faf72dc6dc94f8e5fc1b16351e8d30064f80
Ignacio T <nax_hh@hotmail.com>
2012-05-01 19:49:04 +0200

    a lot of changes

--- neo4j/app2.js
+++ neo4j/app2.js
@@ -7,7 +7,7 @@ var Step = require('./lib/step');
  *  @Vars
  *
  *  var db        -> DB connection object
- *  var nodeCount -> Number of noeds
+ *  var nodeCount -> Number of nodes
  *  var nodes     -> Nodes itself in an object
  *
  *  @Creation
@@ -20,7 +20,7 @@ var Step = require('./lib/step');
  *  .newNode(data, callback)
  *      Create a node and store in nodes var.
  *      @param data     = {'data' : 'value', 'data2' : 'value2'}
- *      @param callback = The callback
+ *      @param callback
  *
  *  .getNode(node, key)
  *      Get data from a specific node
@@ -44,12 +44,14 @@ var Graph = ( function(connect) {
 	var db, nodeCount, nodes;
 
 	this.nodeCount = 0;
-	this.nodes = {};
+	this.nodes = [];
 	this.db = new neo4js.GraphDatabase( process.env.NEO4J_URL || connect);
 });
 
 Graph.prototype.newNode = function(data, callback) {
-	this.nodes[ this.nodeCount ] = new Node( data, this.db, callback );
+	this.nodes[ this.nodeCount ] = this.db.createNode(data);
+	
+	this.nodes[ this.nodeCount ].save( callback );
 	this.nodeCount ++;
 };
 
@@ -58,70 +60,53 @@ Graph.prototype.getNode = function(node, key) {
 		return this.nodes;
 
 	if ( key === undefined) {
-		return this.nodes[node]['data']; //all the data to me!
+		return this.nodes[node]; //all the data to me!
 	}else if (typeof key === 'object' ) {
-		return this.nodes[node]['data'][ key[0] ][ key[1] ]; //search for recursivity
+		return this.nodes[node][ key[0] ][ key[1] ]; //search for recursivity
 	}
-	return this.nodes[node]['data'][key]; //get just the data of the key
+	return this.nodes[node][key]; //get just the data of the key
 };
 
 //del node
+Graph.prototype.delNode = function(node, callback) {
+	var that = this;
+
+	this.nodes[node].del( function(err) {
+		delete that.nodes[node];
+		callback(err, this);
+	});
+};
 
 //create relationship
 Graph.prototype.newRelation = function(node, toNode, type, data, callback) {
-	this.nodes[ node ].newRelation( this.nodes[ toNode ], type, data, callback  );
-};
-
-//get relationship
-//delete relationship
-
-/*
- *  Node abstraction Class
- *	
- *  @vars
- *	
- *  var _node -> Data inside the node
- *	
- *  @Creation
- *	
- *  var node = new Node( data, dbConn, callback )
- *      @param data     = Data to store inside the node
- *      @param dbConn   = Object connection to DB
- *      @param callback
- *
- *  @Methods
- *
- *  .save( callback )
- *      Save the node into the DB
- *      @param callback = The callback to return when it finished
- *	
- *  .newRelation( to, type, data, callback )
- *      Creates a relationship betweet this node and `to` node
- *      @param to       = destination node
- *      @param type     = the relationship type (default 'goes')
- *      @param data     = data to add into the relationship
- *      @param callback
- */
-var Node = ( function(NodeData, dbConn, callback) {
-	var _node;
-
-	this._node = dbConn.createNode(NodeData);
-	this.save(callback);
-});
-
-Node.prototype.save = function(callback) {
-	this._node.save( function(err) {
-		callback(err, this);
-	});
-}
-
-Node.prototype.newRelation = function(to, type, data, callback) {
 	if (type === undefined)
 		type = 'goes';
 	if (data === undefined) 
 		data = {};
 
-	this._node.createRelationshipTo( to._node, type, data, callback);
+	this.nodes[ node ].createRelationshipTo( this.nodes[ toNode ], type, data, callback);
+
+};
+
+//get relationship
+//delete relationship
+
+//get path
+Graph.prototype.getPath = function(from, to, callback) {
+	//neo4js.path( this.nodes[from], this.nodes[to], callback);
+
+	var query = [
+		'START s=node(3), e=node(2)',
+		'MATCH p = shortestPath( s-[*..15]->e )',
+		'RETURN p'
+	].join('\n');
+
+	//this.db.query(query, callback);
+	this.db.query(query, function(err, res) {
+		if (err !== null)
+			console.log(err);
+		console.log(res);
+	});
 }
 
 
@@ -129,15 +114,18 @@ Node.prototype.newRelation = function(to, type, data, callback) {
 
 //connect to db
 var myGraph = new Graph('http://localhost:7474');
+
+
 Step ( //this is just for async get sync.
 	function () { //creating nodes
-		myGraph.newNode( { 'stop' : 'Barcelona' }, this.parallel() );
-		myGraph.newNode( { 'stop' : 'Tarragona' }, this.parallel() );
-		myGraph.newNode( { 'stop' : 'Reus' }, this.parallel());
+		myGraph.newNode( { 'stop' : 'Barcelona' }, this );
+		myGraph.newNode( { 'stop' : 'Tarragona' }, this );
+		myGraph.newNode( { 'stop' : 'Reus' }, this);
 	},
 	function (err, text) { //get some data?
 		//console.log( myGraph.getNode() );
-		//console.log( myGraph.getNode( 0 , ['data', 'db'] ));
+		//console.log( myGraph.getNode( 0 , 'data' ));
+		//console.log( myGraph.getNode( 0 , ['db', 'url'] ));
 		//console.log( myGraph.getNode(0) );
 		return this;
 	},
@@ -145,13 +133,24 @@ Step ( //this is just for async get sync.
 		myGraph.newRelation(0, 1, 'goes', {}, this.parallel() );
 		myGraph.newRelation(1, 2, 'goes', {}, this.parallel() );
 	},
-	function (err, text) { //errors?
-		if ( err !== undefined)
-			console.log(err);
-
-		return this;
+	function (err, text) {
+		myGraph.getPath(0, 1, this);
 	},
 	function (err, text) { //get a path bettween relationships
+		if (err !== null)
+			console.log(err);
+		console.log(text);
+	}/*,
 
-	}
+	function (err, text) {//delete nodes
+		myGraph.delNode(0, this.parallel());
+		myGraph.delNode(1, this.parallel());
+		myGraph.delNode(2, this.parallel());
+	},
+	function (err, text) {
+		if (err !== undefined)
+			console.log(err);
+		console.log( myGraph.getNode().length );
+		//console.log( myGraph.getNode() );
+	}*/
 );
